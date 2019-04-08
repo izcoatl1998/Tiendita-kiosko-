894581575073a2bbabf930ae0ae289f38d78b3db
izcoatl1998 <48344749+izcoatl1998@users.noreply.github.com>
2019-04-08 16:30:09 -0500

    Tiendita Kiosko

---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Mitiendita
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double total, sub_total,pagar,cambio;
+        private void Agregar_Click(object sender, EventArgs e)
+        {
+           // double total, sub_total;
+            string producto = Producto.Text;
+            double precio = Convert.ToDouble(Precio.Text);
+            int cantidad = Convert.ToInt32(Cantidad.Text);
+            Tiendita tienda = new Tiendita(producto, precio, cantidad);
+            double importe = tienda.Importe();//(Precio por cantidad)
+            Importe.Text = Convert.ToString(importe);
+            sub_total =sub_total +importe;
+            Sub_total.Text = Convert.ToString(sub_total);
+            double Iva = tienda.IVA(sub_total);
+            IVA.Text = Convert.ToString(Iva);//Tuve que poner Iva sin mayuscula por que no me tomaba la textbox
+            total = sub_total + Iva;
+            Total.Text = Convert.ToString(total);
+            Carrito.Items.Add(tienda.ToString());
+        }
+
+        private void Ticket_Click(object sender, EventArgs e)
+        {
+            /*No supe como mostrar el total antes de pagar, por que puestra el total ya restandolo el pagar*/
+            saveFileDialog1.ShowDialog();
+            string rutaArchivo;
+            rutaArchivo = saveFileDialog1.FileName;
+            FileStream fs = new FileStream(rutaArchivo, FileMode.Create);
+            StreamWriter ticket = new StreamWriter(fs);
+            ticket.WriteLine("Ticket");
+            ticket.WriteLine("Kiosko" + Environment.NewLine);
+            for(int i = 0; i < Carrito.Items.Count; i++)
+            {
+                ticket.WriteLine(Carrito.Items[i].ToString());
+            }
+            ticket.WriteLine("Subtotal: $"+sub_total.ToString());
+            ticket.WriteLine("Subtotal con Iva del 16%:  $"+total.ToString());
+            ticket.WriteLine("Se pago con: "+pagar.ToString());
+            ticket.WriteLine("Cambio: " + (pagar-total).ToString());
+            ticket.Close();
+            fs.Close();
+        }
+
+        private void Pagar_Click(object sender, EventArgs e)
+        {
+             pagar = Convert.ToDouble(Supago.Text);
+            double cambio;
+            if (pagar < 0)
+            {
+                pagar =0;
+            }
+            if (pagar > total)
+            {
+                cambio = pagar - total;
+                Cambio.Text = Convert.ToString(cambio);
+                cambio = pagar - total;
+                Total.Text = "0";
+            }
+            else
+            {
+                total -= pagar;
+                Cambio.Text = "0";
+                Sub_total.Text = Convert.ToString(sub_total);
+                Total.Text = Convert.ToString(total);
+            }
+           
+            
+        }
+    }
+}
---
+++ Tiendita.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mitiendita
+{
+    class Tiendita
+    {
+        private string _Producto;
+        public int _Cantidad;
+        private double _Total;
+        private double _Precio;
+        public string Producto
+        {
+            get { return _Producto; }
+        }
+        public int Cantidad
+        {
+            get { return _Cantidad; }
+        }
+        public double Total
+        {
+            get { return _Cantidad; }
+        }
+        public double Precio
+        {
+            get { return _Precio; }
+        }
+        public Tiendita(string producto, double precio, int cantidad)
+        {
+            _Producto = producto;
+            _Cantidad = cantidad;
+            _Precio = precio;
+            _Total = precio * cantidad;
+        }
+        public double Importe()
+        {    //El importe se saca multiplicando la cantidad por el precio del producto
+            double Importe = Precio * _Cantidad;
+            return Importe;
+        }
+        public double IVA(double SubT)
+        {       //El iva en mexico es 0.16%
+            double iva = SubT * 0.16;
+            return iva;
+        }
+        public override string ToString()
+        {
+            return "Producto: " + _Producto.ToString() + "  Cantidad: " + _Cantidad.ToString() +"  Precio C/u: $ " + _Precio.ToString() + "  Monto Total: $" + _Total.ToString() + Environment.NewLine;
+
+        }
+    }
+}
